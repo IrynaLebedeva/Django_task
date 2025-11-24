a94b25f3af08c5ac385f26c0c01434606b9232f6
IrynaLebedeva <aist.ira@gmail.com>
2025-11-24 22:03:32 +0100

    added HW_11 in admin

--- task_manager/admin.py
+++ task_manager/admin.py
@@ -2,12 +2,23 @@ from django.contrib import admin
 from .models import Task, SubTask, Category
 
 
+class SubTaskInlineAdmin(admin.TabularInline):
+    model = SubTask
+    extra = 1
+
 @admin.register(Task)
 class TaskAdmin(admin.ModelAdmin):
-    list_display = ("title", "description", "status", "deadline", "created_at")
+    list_display = ("short_title", "description", "status", "deadline", "created_at")
     search_fields = ('title',)
     list_filter = ('status','title')
     ordering = ('-created_at',)
+    inlines = [SubTaskInlineAdmin]
+
+    def short_title(self, obj):
+        if len(obj.title) >10:
+            return obj.title[:10]+"..."
+        return obj.title
+    short_title.short_description = 'Title'
 
 
 @admin.register(SubTask)
@@ -16,6 +27,12 @@ class SubTaskAdmin(admin.ModelAdmin):
     search_fields = ("title", "status")
     list_filter = ("title", "deadline" )
     ordering = ('-created_at',)
+
+    @admin.action(description="Mark Subtasks as Done")
+    def mark_done(self, request, queryset):
+        queryset.update(status="done")
+
+    actions = ["mark_done"]
 
 
 @admin.register(Category)
